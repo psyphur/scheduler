854c4aae9095406eada34eb9b992837222c1f9d5
Shaun Yap <me@shaunyap.ca>
2020-12-14 19:41:27 -0800

    Add tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ import Button from '../Button';
 export default function Form(props) {
   const [ name, setName ] = useState(props.name || "");
   const [ interviewer, setInterviewer ] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   function reset() {
     setName("");
@@ -15,6 +16,16 @@ export default function Form(props) {
   function cancel() {
     reset();
     props.onCancel();
+  }
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    props.onSave(name, interviewer);
+    setError(null);
   }
 
   return (
@@ -30,17 +41,19 @@ export default function Form(props) {
             type="text"
             placeholder="Enter Student Name"
             onChange={event => setName(event.target.value)}
+            data-testid="student-name-input"
             /*
               This must be a controlled component
             */
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={event => (props.onSave(name, interviewer))}>Save</Button>
+          <Button confirm onClick={event => (validate())}>Save</Button>
         </section>
       </section>
     </main>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import InterviewerListItem from './InterviewerListItem';
 
@@ -25,4 +26,8 @@ export default function InterviewerList(props) {
       </ul>
     </section>
   )
+}
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
 }
